dce4c7f44dc83b858d31d15ecdd3ed0db1ef8fc6
Nikhil N <151709586+NikhilN224@users.noreply.github.com>
2024-05-24 09:08:17 +0530

    Time: 53 ms (76.15%), Space: 50.4 MB (59.04%) - LeetHub

---
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function (nums, target) {
+    let res = [];
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            res.push(i);
+        }
+    }
+    return res;
+};
