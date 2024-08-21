c4c3acd0803d53288cafc1898c9caf740642b39c
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-08-21 10:49:03 +0530

    Time: 104 ms (31.34%), Space: 54.8 MB (34%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -4,9 +4,10 @@
  */
 var arrayPairSum = function (nums) {
     nums.sort((a, b) => a - b);
+
     let sum = 0;
     for (let i = 0; i < nums.length; i += 2) {
-        sum += Math.min(nums[i], nums[i + 1]);
+        sum += nums[i]
     }
     return sum
 };
