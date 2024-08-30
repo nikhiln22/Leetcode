ff3ca1aee76060c2b4e06431fac5d3749a055c93
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-08-30 13:44:16 +0530

    Time: 66 ms (16.18%), Space: 50.9 MB (54.75%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -1,13 +1,18 @@
-var longestPalindrome = function(s) {
+var longestPalindrome = function (s) {
     let set = new Set();
     let count = 0;
-    for(let i=0;i<s.length;i++){
-        if(set.has(s[i])){
-            count +=2;
+    for (let i = 0; i < s.length; i++) {
+        if (set.has(s[i])) {
+            count += 2;
             set.delete(s[i]);
-        }else {
+        } else {
             set.add(s[i]);
         }
     }
+    console.log('set:', set);
+    console.log('count:', count)
+    console.log('set.size:', set.size);
+    let output = count + set.size
+    console.log('output:', output);
     return count + ((set.size > 0) ? 1 : 0);
 };
