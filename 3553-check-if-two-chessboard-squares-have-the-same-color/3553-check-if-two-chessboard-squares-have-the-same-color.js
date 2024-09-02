60108b5fea159dbb7526b560e817fdbe965f1b14
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-09-02 16:20:38 +0530

    Time: 54 ms (100%), Space: 49.1 MB (100%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.js
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.js
@@ -7,14 +7,10 @@ var checkTwoChessboards = function (coordinate1, coordinate2) {
     let firstValue = coordinate1.split('');
     let secondValue = coordinate2.split('');
     firstValue[0] = coordinate1.charCodeAt(0);
-    console.log('firstValue:', firstValue);
     secondValue[0] = coordinate2.charCodeAt(0);
-    console.log('secondValue:', secondValue);
 
     sumRowCoordinate = parseInt(firstValue[0]) + parseInt(secondValue[0]);
-    console.log('sumRowCoordinate:', sumRowCoordinate);
     sumColumnCoordinate = parseInt(firstValue[1]) + parseInt(secondValue[1]);
-    console.log('sumColumnCoordinate:', sumColumnCoordinate);
 
     return (sumRowCoordinate % 2 === sumColumnCoordinate % 2)
 };
