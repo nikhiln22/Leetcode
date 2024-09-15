16d2513927ec413cbd7b940518d0798a33d917a1
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-09-15 21:46:08 +0530

    Time: 49 ms (92.32%), Space: 50.7 MB (91.6%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            if ((i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+                n--;
+                i++;
+            }
+        }
+    }
+    return n <= 0
+};
