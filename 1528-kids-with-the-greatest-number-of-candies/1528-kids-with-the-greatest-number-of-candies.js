c9590794ae8e2fe4910aea907e31c3ec70a026df
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-09-15 21:44:25 +0530

    Time: 65 ms (18.76%), Space: 50 MB (82.64%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    let output = [];
+    let max=Math.max(...candies)
+    for (i = 0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >=max ) {
+            output.push(true);
+        } else
+            output.push(false);
+    }
+    return output;
+    
+};
