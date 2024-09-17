e235165e7e52fa12e7030c0fc55ac9264e6336b1
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-09-17 16:28:34 +0530

    Time: 53 ms (76.75%), Space: 49.9 MB (83.88%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -1,25 +1,12 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
 var intersect = function (nums1, nums2) {
-    let count = {};
-    let result = [];
+    let arr = [];
 
-    for (let num of nums1) {
-        if (count[num]) {
-            count[num]++;
-        } else {
-            count[num] = 1;
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+            arr.push(nums1[i]);
+            nums2.splice(nums2.indexOf(nums1[i]), 1);
         }
     }
 
-    for (let num of nums2) {
-        if (count[num] > 0 && count[num]) {
-            result.push(num);
-            count[num]--;
-        }
-    }
-    return result
+    return arr;
 };
