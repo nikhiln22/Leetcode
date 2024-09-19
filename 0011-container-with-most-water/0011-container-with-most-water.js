46776316740fbd5ea59fd531dbe6523bf71400ef
Nikhil N <151709586+nikhiln22@users.noreply.github.com>
2024-09-19 07:39:22 +0530

    Time: 65 ms (67.66%), Space: 57.3 MB (35.05%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    while (left < right) {
+        const width = right - left;
+        const area = Math.min(height[left], height[right]) * width;
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea
+};
